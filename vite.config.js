782a0acae24bc03b593660d6518088623ff80832
AlejandroBuitragoB <134160737+AlejandroBuitragoB@users.noreply.github.com>
2023-11-26 11:13:28 -0300

    añadidos plugs y archivos de vite

--- vite.config.js
+++ vite.config.js
@@ -6,42 +6,18 @@ export default defineConfig({
   plugins: [
     { enforce: 'pre', ...mdx() },
     react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
+    react({ jsxImportSource: '@emotion/react' }),
+    react({ jsxRuntime: 'classic' }),
+    react({
+      babel: {
+        presets: [...],
+        // Your plugins run before any built-in transform (eg: Fast Refresh)
+        plugins: [...],
+        // Use .babelrc files
+        babelrc: true,
+        // Use babel.config.js files
+        configFile: true,
+      }
+    })
   ],
-})
-
-react({ jsxImportSource: '@emotion/react' })
-
-react({ jsxRuntime: 'classic' })
-
-react({
-  babel: {
-    presets: [...],
-    // Your plugins run before any built-in transform (eg: Fast Refresh)
-    plugins: [...],
-    // Use .babelrc files
-    babelrc: true,
-    // Use babel.config.js files
-    configFile: true,
-  }
-})
-
-react({
-  babel: {
-    parserOpts: {
-      plugins: ['decorators-legacy'],
-    },
-  },
-})
-
-app.get('/', async (req, res, next) => {
-  try {
-    let html = fs.readFileSync(path.resolve(root, 'index.html'), 'utf-8')
-
-    // Transform HTML using Vite plugins.
-    html = await viteServer.transformIndexHtml(req.url, html)
-
-    res.send(html)
-  } catch (e) {
-    return next(e)
-  }
 })
