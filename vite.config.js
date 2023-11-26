b3a0eb7db9d2b845058abe782b3d284d3ef64a19
AlejandroBuitragoB <134160737+AlejandroBuitragoB@users.noreply.github.com>
2023-11-26 11:28:01 -0300

    swc plugs

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import mdx from '@mdx-js/rollup'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [
-    { enforce: 'pre', ...mdx() },
-    react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
-    react({ jsxImportSource: '@emotion/react' }),
-    react({ jsxRuntime: 'classic' }),
-    react({
-      babel: {
-        presets: [...],
-        // Your plugins run before any built-in transform (eg: Fast Refresh)
-        plugins: [...],
-        // Use .babelrc files
-        babelrc: true,
-        // Use babel.config.js files
-        configFile: true,
-      }
-    })
-  ],
-})
+  plugins: [react()],
+});
